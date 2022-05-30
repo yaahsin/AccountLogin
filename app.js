e4863b9d12d5482eaa5b0d3a6893ca6b1d8bd4f0
yaahsin <brillanteoboe@gmail.com>
2022-05-30 22:04:33 +0800

    feat: login success

--- app.js
+++ app.js
@@ -3,7 +3,34 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
-
+const userlist = [
+  {
+    firstName: 'Tony',
+    email: 'tony@stark.com',
+    password: 'iamironman'
+  },
+  {
+    firstName: 'Steve',
+    email: 'captain@hotmail.com',
+    password: 'icandothisallday'
+  },
+  {
+    firstName: 'Peter',
+    email: 'peter@parker.com',
+    password: 'enajyram'
+  },
+  {
+    firstName: 'Natasha',
+    email: 'natasha@gamil.com',
+    password: '*parol#@$!'
+  },
+  {
+    firstName: 'Nick',
+    email: 'nick@shield.com',
+    password: 'password'
+  }
+]
+let message = ""
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -18,8 +45,19 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   const { account, password } = req.body
-  console.log(`account: ${account} password: ${password}`)
-  res.render('index')
+  const user = userlist.find(user => user.email === account)
+  if (user) {
+    if (user.password === password) {
+      const firstName = user.firstName
+      console.log(firstName)
+      res.render('success', { firstName })
+    } else {
+      message = "Wrong password.Try again or ..."
+    }
+  } else {
+    message = "Could't find your account, please check again ><"
+    res.render('index', { message })
+  }
 })
 
 app.listen(port, () => {
