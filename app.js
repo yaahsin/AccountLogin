7682ae284cba06e026c40e93f11e3ac60d9e8ee1
yaahsin <brillanteoboe@gmail.com>
2022-05-30 22:48:40 +0800

    userlist: object to joson

--- app.js
+++ app.js
@@ -3,35 +3,9 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
-const userlist = [
-  {
-    firstName: 'Tony',
-    email: 'tony@stark.com',
-    password: 'iamironman'
-  },
-  {
-    firstName: 'Steve',
-    email: 'captain@hotmail.com',
-    password: 'icandothisallday'
-  },
-  {
-    firstName: 'Peter',
-    email: 'peter@parker.com',
-    password: 'enajyram'
-  },
-  {
-    firstName: 'Natasha',
-    email: 'natasha@gamil.com',
-    password: '*parol#@$!'
-  },
-  {
-    firstName: 'Nick',
-    email: 'nick@shield.com',
-    password: 'password'
-  }
-]
-let message = ""
+const userlist = require('./userlist.json')
 const port = 3000
+let message = ""
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
